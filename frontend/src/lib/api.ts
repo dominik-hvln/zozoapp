1ec859a63c15eb53bf404c8660829c945003f556
Dominik Kowalski <dominik@dkowalski.pl>
2025-08-11 17:14:42 +0200

    update: working base app

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -2,13 +2,14 @@ import axios from 'axios';
 import { useAuthStore } from '@/store/auth.store';
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3001',
+    // Ten kod dynamicznie wybierze adres URL
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001',
 });
 
+// Interceptor dołączający token (pozostaje bez zmian)
 api.interceptors.request.use(
     (config) => {
         const token = useAuthStore.getState().token;
-
         if (token) {
             config.headers['Authorization'] = `Bearer ${token}`;
         }
