ec3b4e0be2af8cef8638b1368970d23d772f3126
Dominik Kowalski <dominik@dkowalski.pl>
2025-08-11 17:39:57 +0200

    update: update API

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: 'https://zozoapp.vercel.app/',
+        origin: 'https://zozoapp-dlcovl3uo-hvln.vercel.app/',
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         credentials: true,
     });
