6f41ae539d0c887c4e8d50c99d12af5bc3628f8f
Dominik Kowalski <dominik@dkowalski.pl>
2026-04-14 11:56:19 +0200

    add: coolify

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,9 +1,16 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, Logger } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+    private readonly logger = new Logger(PrismaService.name);
+
     async onModuleInit() {
-        await this.$connect();
+        try {
+            await this.$connect();
+            this.logger.log('Pomyślnie połączono z bazą danych Prisma.');
+        } catch (error) {
+            this.logger.error('Nie udało się połączyć z bazą danych na start z powodu błędu sieciowego, ale serwer zostaje uruchomiony dalej.', error);
+        }
     }
 }
