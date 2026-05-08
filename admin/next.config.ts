56f10c3ecbac3930a4ec71eb6f0fc1cd03c52f1e
Dominik Kowalski <dominik@dkowalski.pl>
2026-05-08 14:57:01 +0200

    fix

--- admin/next.config.ts
+++ admin/next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
                 port: '',
                 pathname: '/storage/v1/object/public/**',
             },
+            {
+                protocol: 'https',
+                hostname: 'supabase-api.zozoapp.pl',
+                port: '',
+                pathname: '/storage/v1/object/public/**',
+            },
         ],
     },
 };
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -25,6 +25,12 @@ const nextConfig: NextConfig = {
                 port: '',
                 pathname: '/storage/v1/object/public/**',
             },
+            {
+                protocol: 'https',
+                hostname: 'supabase-api.zozoapp.pl',
+                port: '',
+                pathname: '/storage/v1/object/public/**',
+            },
         ],
     },
 };
